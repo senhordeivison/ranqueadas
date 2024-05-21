5254ddae36eb8b2cf4e16b9fb2aea2cc63fd6e07
Deivison <senhordeivison@gmail.com>
2024-05-21 09:27:55 -0300

    funções

---
+++ main.js
@@ -0,0 +1,32 @@
+function ranque(vitorias, derrotas) {
+    resultado = vitorias - derrotas
+    return resultado
+}
+
+let vitorias = ranque(348, 269)
+
+let nivel = ''
+
+if (vitorias <= 10) {
+    nivel = 'Ferro'
+}
+else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = 'Bronze'
+}
+else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = 'Prata'
+}
+else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = 'Ouro'
+}
+else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = 'Diamante'
+}
+else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = 'Lendário'
+}
+else {
+    nivel = 'Imortal'
+}
+
+console.log(`O herói tem o saldo de *${vitorias} vitórias* e está no nível: ${nivel}.`)
